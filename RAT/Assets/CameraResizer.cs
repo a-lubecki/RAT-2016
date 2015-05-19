5c182d7b1844cb70eb6abc9c6e1e974dbd59131d
alubecki <aurel.lubecki@gmail.com>
2015-05-20 00:06:49 +0200

    Fixed the fucking glitch by applying a mothafucking amazing scaling stuff on the camera to apply a perfect pixel perfect baby !!!

--- RAT/Assets/CameraResizer.cs
+++ RAT/Assets/CameraResizer.cs
@@ -1,45 +1,70 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+// http://docs.unity3d.com/Manual/class-Camera.html
+/**
+ * The CameraResizer adapt the screen to fit several tiles (nbVisibleTiles) in the screen height regardless of the screen size.
+ * The tile size will be scaled by steps (32 > 64 > 96 > ...) and the screen viewport (only height) will be resized to avoid glitches.
+ * If the tile size reaches the minimum (32px) the size won't be reduced and the nbVisibleTiles field may be ignored.
+ */
 public class CameraResizer : MonoBehaviour {
 	
-	public int nbTilesInCamHeight = 1;
-	private int lastNbTilesInCamHeight = 0;
-	private int lastHeightMultiplier = 0;
+	[Tooltip("Nb min of visible tiles in the screen, may be ignored if too big, can't be 0")]
+	public int nbVisibleTiles;
 
 	void Start () {
-
-		Update();
+		
+		scaleScene();
 	}
 
 	void Update () {
-		/*
-		if(nbTilesInCamHeight <= 0) {
-			nbTilesInCamHeight = 1;
+
+		if (Input.GetKeyDown(KeyCode.F)) {
+			Screen.fullScreen = !Screen.fullScreen;//TEST with fullscreen
 		}
 
-		int tilesHeightToDisplay = nbTilesInCamHeight * Constants.TILE_SIZE;
+		scaleScene();
+	}
 
-		int heightMultiplier = Screen.height / tilesHeightToDisplay;//no float
-		if(heightMultiplier < 1) {
-			heightMultiplier = 1;
+	private void scaleScene() {
+
+		if(nbVisibleTiles <= 0) {
+			throw new System.InvalidOperationException();
 		}
 
-		if(heightMultiplier != lastHeightMultiplier || 
-		   nbTilesInCamHeight != lastNbTilesInCamHeight) {//update
 
-			lastHeightMultiplier = heightMultiplier;
-			lastNbTilesInCamHeight = nbTilesInCamHeight;
+		int minHeightToDisplay = nbVisibleTiles * Constants.TILE_SIZE;
 
-			Camera cam = GetComponent<Camera>();
-			
-			float newSize = heightMultiplier * nbTilesInCamHeight * 0.5f + 
-				((float)(Screen.height / (float)tilesHeightToDisplay) - heightMultiplier) * 0.5f;
+		float ratio = Screen.height / (float)minHeightToDisplay;
 
-			cam.orthographicSize = newSize; //TODO animate resizing
-
+		int multiplier = (int)Mathf.Floor(ratio);
+		if(multiplier <= 0) {
+			multiplier = 1;
 		}
-*/
+
+		int newScreenHeight = Screen.height;
+
+		int divider = 2 * multiplier;
+
+		int nbSparePixels = Screen.height % divider;
+		if(nbSparePixels > 0) {
+			newScreenHeight -= nbSparePixels;
+		}
+		
+		
+		Camera cam = GetComponent<Camera>();
+
+		//set the viewport rect to have a pixel perfect calculation of the orthographic size,
+		// it must be a multiplier of the pixel size,
+		// ex : if the pixel size is 5, if the cam height is 524 => new height will be 520, spare pixels will be 4
+		//spare pixels will be on top of the hud (not a big deal)
+		cam.pixelRect = new Rect(0, 0, Screen.width, newScreenHeight);
+
+		//set the orthographic size to scale the scene with the multiplier
+		cam.orthographicSize = newScreenHeight / (float)divider;
+
+		//Debug.Log(">>> " + Screen.height + " > " + newScreenHeight + " > " + cam.orthographicSize + " > " + multiplier + " > " + nbSparePixels);
+
 
 	}
 }
