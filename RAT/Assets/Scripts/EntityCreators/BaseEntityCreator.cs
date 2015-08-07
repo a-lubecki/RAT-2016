e93b42cf193467e6654ffb5a23b278d75949e205
alubecki <aurel.lubecki@gmail.com>
2015-08-08 00:33:04 +0200

    Fixed bug with tiles sorting order

--- RAT/Assets/Scripts/EntityCreators/BaseEntityCreator.cs
+++ RAT/Assets/Scripts/EntityCreators/BaseEntityCreator.cs
@@ -100,6 +100,7 @@ public abstract class BaseEntityCreator {
 				spriteRenderer.sortingLayerName = sortingLayerName;
 			}
 
+			spriteRenderer.sortingOrder = orderInLayer;
 		}
 
 		//display debug image
