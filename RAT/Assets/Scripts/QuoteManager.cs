b5bfe8e73568b7c417f3fc76215fb0ee1749d575
alubecki <aurel.lubecki@gmail.com>
2015-12-28 13:16:28 +0100

    Added several quotes

--- RAT/Assets/Scripts/QuoteManager.cs
+++ RAT/Assets/Scripts/QuoteManager.cs
@@ -8,22 +8,54 @@ public class QuoteManager : MonoBehaviour {
 	public static bool TEST = false;
 
 	private static readonly Dictionary<string, string> authors = new Dictionary<string, string>() {
-		{"GANDHI", "Gandhi"},
-		{"AUGUSTE_COMPTE", "Auguste Compte"},
-		{"PIERRE_CURIE", "Pierre Curie"},
 		{"ALBERT_EINSTEIN", "Albert Einstein"},
-		{"EDWARD_D_MORRISON", "Edward D. Morrison"}
+		{"ALDOUS_HUXLEY", "Aldous Huxley"},
+		{"AUGUSTE_COMPTE", "Auguste Compte"},
+		{"CLAUDE_JOSEPH_ROUGET_DE_LISLE", "Claude Joseph Rouget de Lisle"},
+		{"DAVID_HOMEL", "David Homel"},
+		{"EDGAR_MORIN", "Edgar Morin"},
+		{"EDWARD_D_MORRISON", "Edward D. Morrison"},
+		{"FRIEDRICH_NIETZSCHE", "Friedrich Nietzsche"},
+		{"GANDHI", "Gandhi"},
+		{"HENRY_DE_MONTHERLANT", "Henry de Montherlant"},
+		{"MICHAEL_CRICHTON", "Michael Crichton"},
+		{"NAPOLEON_BONAPARTE", "Napoléon Bonaparte"},
+		{"PIERRE_CURIE", "Pierre Curie"},
+		{"RENE_DUBOS", "René Dubos"},
+		{"TIM_WHITE", "Tim White"},
+		{"WOODY_ALLEN", "Woody Allen"}
 	};
 
 	private static readonly string[] quoteTrKeys = new string[] {
+		"Quote.ALBERT_EINSTEIN.0",
+		"Quote.ALBERT_EINSTEIN.1",
+		"Quote.ALBERT_EINSTEIN.2",
+		"Quote.ALBERT_EINSTEIN.3",
+		"Quote.ALDOUS_HUXLEY.0",
+		"Quote.ANONYMOUS.0",
 		"Quote.AUGUSTE_COMPTE.0",
+		"Quote.CLAUDE_JOSEPH_ROUGET_DE_LISLE.0",
+		"Quote.DAVID_HOMEL.0",
+		"Quote.EDGAR_MORIN.0",
+		"Quote.EDGAR_MORIN.1",
+		"Quote.EDWARD_D_MORRISON.0",
+		"Quote.FRIEDRICH_NIETZSCHE.0",
 		"Quote.GANDHI.0",
 		"Quote.GANDHI.1",
 		"Quote.GANDHI.2",
 		"Quote.GANDHI.3",
 		"Quote.GANDHI.4",
 		"Quote.GANDHI.5",
-		"Quote.PIERRE_CURIE.0"
+		"Quote.HENRY_DE_MONTHERLANT.0",
+		"Quote.HENRY_DE_MONTHERLANT.1",
+		"Quote.HENRY_DE_MONTHERLANT.2",
+		"Quote.HENRY_DE_MONTHERLANT.3",
+		"Quote.MICHAEL_CRICHTON.0",
+		"Quote.NAPOLEON_BONAPARTE.0",
+		"Quote.PIERRE_CURIE.0",
+		"Quote.RENE_DUBOS.0",
+		"Quote.TIM_WHITE.0",
+		"Quote.WOODY_ALLEN.0"
 	}; 
 
 	
@@ -55,11 +87,18 @@ public class QuoteManager : MonoBehaviour {
 		string key = getAuthorKey(quoteTrKey);
 		string authorName = null;
 
-		foreach(KeyValuePair<string, string> entry in authors) {
+		if(key.Equals("ANONYMOUS")) {
 
-			if(key.Equals(entry.Key)) {
-				authorName = entry.Value;
-				break;
+			authorName = Constants.tr("Author.ANONYMOUS");
+
+		} else {
+
+			foreach(KeyValuePair<string, string> entry in authors) {
+
+				if(key.Equals(entry.Key)) {
+					authorName = entry.Value;
+					break;
+				}
 			}
 		}
 
@@ -76,7 +115,7 @@ public class QuoteManager : MonoBehaviour {
 
 	private IEnumerator launchLevel() {
 
-		yield return new WaitForSeconds(6);
+		yield return new WaitForSeconds(5);
 
 		//load the current level
 		if(!GameSaver.Instance.loadCurrentLevel()) {
